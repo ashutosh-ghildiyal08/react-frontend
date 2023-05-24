4626de082e1a4c08eaeb40d14a6b4a2fd44ec679
ashutosh-ghildiyal08 <ashutosh-ghildiyal@globallogic.com>
2023-05-24 09:30:12 +0530

    first commit

---
+++ src/page/Login.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const postData = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:8080/login", {
+        email,
+        password,
+      })
+      .then((res) => {
+        if (res.data === "") {
+          setError("Wrong Credentials!");
+          navigate("/login");
+        } else {
+          navigate("/");
+        }
+      });
+  };
+  return (
+    <section className="mt-5 pt-5">
+      <div className="container-fluid h-custom">
+        <div className="row d-flex justify-content-center align-items-center">
+          <div className="col-md-9 col-lg-6 col-xl-5">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+              className="img-fluid"
+              alt="Sample image"
+            />
+          </div>
+          <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+            <div className="text-danger">{error && error}</div>
+            <form>
+              <h1>Login</h1>
+              <div className="form-outline mb-4">
+                <input
+                  type="email"
+                  id="form3Example3"
+                  className="form-control form-control-lg"
+                  placeholder="Enter a valid email address"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <label className="form-label">Email address</label>
+              </div>
+
+              <div className="form-outline mb-3">
+                <input
+                  type="password"
+                  id="form3Example4"
+                  className="form-control form-control-lg"
+                  placeholder="Enter password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <label className="form-label">Password</label>
+              </div>
+
+              <div className="text-center text-lg-start mt-4 pt-2">
+                <button
+                  onClick={postData}
+                  type="button"
+                  className="btn btn-primary btn-lg"
+                >
+                  Login
+                </button>
+                <p className="small fw-bold mt-2 pt-1 mb-0">
+                  Don't have an account?{" "}
+                  <a href="#!" className="link-danger">
+                    Register
+                  </a>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Login;
